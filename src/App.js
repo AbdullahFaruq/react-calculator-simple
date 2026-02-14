cb0db5425bc44eeaf83930d9dcd94c283e8bd60b
abdallah7778 <124801211+abdallah7778@users.noreply.github.com>
2026-02-14 20:54:33 +0300

    added readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
 
-export default App;
+const Calculator = function () {
+  const [inp1, setInp1] = useState("");
+  const [inp2, setInp2] = useState("");
+  const [sign, setSign] = useState("+");
+  let result;
+  if (sign === "+") {
+    result = inp1 + inp2;
+  }
+  if (sign === "-") {
+    result = inp1 - inp2;
+  }
+  if (sign === "x") {
+    result = inp1 * inp2;
+  }
+  if (sign === "/") {
+    if (inp2 !== 0) {
+      result = (inp1 / inp2).toFixed(2);
+    } else {
+      result = "Error"; // Prevent division by zero
+    }
+  }
+
+  function handleClear() {
+    setInp1("");
+    setInp2("");
+    setSign("+");
+  }
+
+  return (
+    <div className="calc">
+      <input
+        type="text"
+        value={inp1}
+        onChange={(e) => setInp1(Number(e.target.value))}
+      />
+      <input
+        type="text"
+        value={inp2}
+        onChange={(e) => setInp2(Number(e.target.value))}
+      />
+      <select value={sign} onChange={(e) => setSign(e.target.value)}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="x">x</option>
+        <option value="/">/</option>
+      </select>
+      <h2>{!result ? "result" : result}</h2>
+      <div></div>
+      <button onClick={handleClear}>clear</button>
+    </div>
+  );
+};
